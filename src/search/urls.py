0bff80360a9d19622480543cc878d7e6357bbcc3
BlubberySeal <72081453+BlubberySeal@users.noreply.github.com>
2022-03-28 16:12:01 +0800

    Created and linked temporary html files

--- src/search/urls.py
+++ src/search/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (
-    SearchView
+    SearchView, AdvSearchView
 )
 
 app_name = 'search'
 urlpatterns = [
     path('', SearchView, name='search'),
+    path('adv/', AdvSearchView, name='advsearch'),
 ]
--- src/search/views.py
+++ src/search/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def SearchView(request):
     return render(request, 'search.html', {})
+
+
+def AdvSearchView(request):
+    return render(request, 'advsearch.html')
