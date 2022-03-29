3923d1d6f8ecc8dbf11867aae061b32be15bbcdc
pseudozw <2001zhouwei@gmail.com>
2022-03-29 11:58:54 +0800

    to help in making models later

--- src/search/views.py
+++ src/search/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from .forms import AdvancedSearchForm, SimpleSearchForm
+from .utils import simple_search, adv_search
 
-# Create your views here.
 def SearchView(request):
-    return render(request, 'search.html', {})
+    if request.method == 'POST':
+        form = SimpleSearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            author = form.cleaned_data.get('author')
+            isbn = form.cleaned_data.get('isbn')
+            bid = form.cleaned_data.get('bid')
+            genres = form.cleaned_data.get('genres')
+            
+    else:
+        form = SimpleSearchForm()
+    context = {'form': form}
+    return render(request, 'search.html', context)
 
 
 def AdvSearchView(request):
-    return render(request, 'advsearch.html')
+    if request.method == 'POST':
+        form = AdvancedSearchForm(request.POST)
+        if form.is_valid():
+            keywords = form.cleaned_data.get('keywords')
+            plot = form.cleaned_data.get('plot')
+    else: 
+        form = AdvancedSearchForm()
+    context = {'form': form}
+    return render(request, 'advsearch.html', context)
